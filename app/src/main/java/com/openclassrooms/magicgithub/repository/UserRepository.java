8bf61f39fbff93863d4379a14b839b7b0b9356ad
Gumpah <vickinzi@yahoo.fr>
2021-10-22 17:11:07 +0200

    Deleting TODO mentions

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -7,24 +7,21 @@ import java.util.List;
 
 public class UserRepository {
 
-    private final ApiService apiService; // TODO: A utiliser
+    private final ApiService apiService;
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
     }
 
-    public List<User> getUsers() {
-        // TODO: A modifier
+    public List<User> getUsers() { // Modified
         return apiService.getUsers();
     }
 
-    public void generateRandomUser() {
+    public void generateRandomUser() { // Modified
         apiService.generateRandomUser();
-        // TODO: A modifier
     }
 
-    public void deleteUser(User user) {
+    public void deleteUser(User user) { // Modified
         apiService.deleteUser(user);
-        // TODO: A modifier
     }
 }
